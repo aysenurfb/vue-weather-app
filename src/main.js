5dc31ad40890a149b94b11946cd1767b5289966a
aktepeayse96 <aktepeayse96@gmail.com>
2023-09-26 22:10:16 +0300

    first

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+
+//d711a983698285e8eb88492a02c305ff token
